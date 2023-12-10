bb2971a7e4f713f3ecd43c50341184b9a1b9a5ff
anastasiya.tsymanovich <anastasiya.tsymanovich@zealicsolutions.com>
2023-12-10 16:13:16 +0300

    feat(task-6): implement basic task 6

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  product: "https://d8600njvu8.execute-api.eu-west-1.amazonaws.com/prod",
+  product: "https://k1p1cvfla2.execute-api.eu-west-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://toowbzceo9.execute-api.eu-west-1.amazonaws.com/prod",
+  import: "https://9kz3doh8j1.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
